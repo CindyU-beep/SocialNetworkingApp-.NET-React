43b4a11e7230d1ce886b726acd571a7ef8c28c81
Cindy <cium2295@uni.sydney.edu.au>
2022-10-16 02:04:03 +1100

    creating the database

--- Activities/API/Program.cs
+++ Activities/API/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -13,7 +16,26 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope(); //after method complete, scope is disposed of
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate(); //creates the database if not already exist
+                
+            }
+            catch (Exception e)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(e, "Error occured during database migration");
+            }
+
+            //run the application
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
